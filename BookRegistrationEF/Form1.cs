c1c2c053ae4dc39cbbeda32e966b74587d1ed3af
caseygruse.222 <caseygruse.222@gmail.com>
2018-07-25 14:30:58 -0700

    test code for selecting books

--- BookRegistrationEF/Form1.cs
+++ BookRegistrationEF/Form1.cs
@@ -16,5 +16,22 @@ namespace BookRegistrationEF
 		{
 			InitializeComponent();
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			List<Book> books = BookDB.GetAllBooks();
+
+			foreach(Book b in books)
+			{
+				PopulateBookComboBox(books);
+			}
+		}
+
+		private void PopulateBookComboBox(List<Book> books)
+		{
+			cboBookList.DataSource = books;
+			cboBookList.DisplayMember = "Title";  // title is a string representation of the property I want to display the object with.
+												  // nameof(Book.Title);
+		}
 	}
 }
