c102536cd51263e5d470b4f5fea5705349e3ee8e
caseygruse.222 <caseygruse.222@gmail.com>
2018-07-25 14:13:19 -0700

    added a getAllBooks method and a getAllCustomers method to bookDB and CustomerDB

---
+++ BookRegistrationEF/CustomerDB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRegistrationEF
+{
+	static class CustomerDB
+	{
+		public static List<Customer> GetAllCustomers()
+		{
+
+			BookContext context = new BookContext();
+			
+
+			return context.Customer.ToList();
+		}
+	}
+}
